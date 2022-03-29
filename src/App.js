49169de0920e23dc6facd7e6957d13b6e6a0d21b
eudeslimaar <eudesfilho1994@outlook.com>
2022-03-29 12:24:54 -0300

    interfaz update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 /* eslint-disable react/react-in-jsx-scope */
 
+import Navbar from './components/navbar/Navbar'
 function App () {
   return (
     <div className="App">
+      <Navbar/>
     </div>
   )
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Navbar = () => {
+  return (
+    <>
+      <Container>
+        <Row className='justify-content-md-center'>
+          <Col xs lg='auto'>
+            1 of 3
+          </Col>
+          <Col md='auto'>Variable width content</Col>
+          <Col xs lg='2'>
+            3 of 3
+          </Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col md='auto'>Variable width content</Col>
+          <Col xs lg='2'>
+            3 of 3
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default Navbar
